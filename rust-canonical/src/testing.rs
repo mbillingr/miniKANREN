41fa1ac9348d085f4cfe4e8403c24ec9809ba2d2
Martin Billinger <mbillingr@users.noreply.github.com>
2021-01-17 15:17:30 +0100

    better assertion messages

--- rust-canonical/src/testing.rs
+++ rust-canonical/src/testing.rs
@@ -5,13 +5,17 @@ use crate::goals::StatSubs;
 /// Assert that a goal fails
 pub fn fails(goal: impl Goal<StatSubs>) {
     let result = run!(1, q, goal);
-    assert!(result.is_empty());
+    if !result.is_empty() {
+        panic!("Expected goal to fail, but it succeeded with {:?}", result);
+    }
 }
 
 /// Assert that a goal succeeds at least once
 pub fn succeeds(goal: impl Goal<StatSubs>) {
     let result = run!(1, q, goal);
-    assert!(!result.is_empty());
+    if result.is_empty() {
+        panic!("Goal did not succeed.")
+    }
 }
 
 pub fn has_unique_solution(mut solutions: impl Iterator<Item = Value>, expected: Value) {
